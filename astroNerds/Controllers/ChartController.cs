5a93b22d436f248a5a9780ce589dd686b66ae426
jjuli <jjuli@DESKTOP-PQ2B9LG>
2022-12-12 15:22:55 +0100

    Add project files.

---
+++ astroNerds/Controllers/ChartController.cs
@@ -0,0 +1,22 @@
+﻿using astroNerds.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace astroNerds.Controllers
+{ 
+    public class ChartController : Controller
+{
+    private readonly ILogger<ChartController> _logger;
+
+    public ChartController(ILogger<ChartController> logger)
+    {
+        _logger = logger;
+    }
+
+        public IActionResult Chart()
+        {
+            return View();
+        }
+
+    }
+}
